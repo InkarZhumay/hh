5e265b68580b7af7906354f87ce1385d44351da7
InkarZhumay <inkar.74@mail.ru>
2023-08-11 12:53:52 +0500

    message

--- config/config.js
+++ config/config.js
@@ -10,7 +10,7 @@ module.exports = {
         username: 'doadmin',
         password: 'AVNS_rpeKir_-Tm2jUSlSk0S',
         database: 'defaultdb',
-        host: 'db-postgresql-sgp1-28207-do-user-14482502-0.b.db.ondigitalocean.com',
+        host: 'private-db-postgresql-sgp1-28207-do-user-14482502-0.b.db.ondigitalocean.com',
         dialect: 'postgres',
         port: 25060
     },
