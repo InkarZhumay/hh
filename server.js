b5046442bdbbb639ff6d9b6e07342d52b4df1f53
InkarZhumay <inkar.74@mail.ru>
2023-08-14 11:00:05 +0500

    message

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const logger = require('morgan');
 const passport = require('passport');
+const cors = require('cors')
 // const multer = require('multer')
 // const upload = multer()
 const app = express();
 
 // app.request(logger('dev'))
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.urlencoded());
 app.use(express.json());
 app.use(express.static(__dirname + "/public"))
